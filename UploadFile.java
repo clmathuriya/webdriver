e6aa55bb9f6b95f318b9d2e4c91610a8b4333b11
chhagan Mathuriya <clmathuriya@gmail.com>
2015-07-23 03:15:48 +0530

    Create UploadFile.java
    File upload example

---
+++ UploadFile.java
@@ -0,0 +1,28 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * 
+ * @author cl
+ *
+ */
+public class UploadFile {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://how-old.net/");
+		((JavascriptExecutor) driver)
+				.executeScript(
+						"arguments[0].style.visibility = 'visible'; arguments[0].style.height = '1px'; arguments[0].style.width = '1px'; arguments[0].style.opacity = 1",
+						driver.findElement(By.id("uploadBtn")));
+		driver.findElement(By.id("uploadBtn")).sendKeys(
+				"C:\\chhagan\\26jan.jpeg");
+
+	}
+
+}
