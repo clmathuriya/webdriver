e98ac06ee729906fd8eecf7ee68bfa30fa9862bf
chhagan mathuriya <chhaganlal.m@plancess.com>
2015-10-08 19:26:37 +0530

    to run headless firefox

--- src/main/java/com/plancess/selenium/tests/BaseTest.java
+++ src/main/java/com/plancess/selenium/tests/BaseTest.java
@@ -1,9 +1,12 @@
 package com.plancess.selenium.tests;
 
+import java.io.File;
 import java.lang.reflect.Method;
 
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -47,7 +50,7 @@ public class BaseTest {
 
 	@Parameters({ "host_ip", "port", "os", "browser", "browserVersion", "useProxy" })
 	@BeforeMethod
-	public void setUp(@Optional("localhost") String host, @Optional("4444") String port, @Optional("WINDOWS") String os,
+	public void setUp(@Optional("localhost") String host, @Optional("4444") String port, @Optional("LINUX") String os,
 			@Optional("firefox") String browser, @Optional("40.0") String browserVersion,
 			@Optional("false") String useProxy, @Optional Method method) {
 
@@ -64,11 +67,25 @@ public class BaseTest {
 		// capabilities.setCapability("version", browserVersion);
 		capabilities.setCapability("platform", Platform.valueOf(os));
 		executor = new Executioner();
-		this.driver = executor.openBrowser(host, port, capabilities);
+		if (browser.equalsIgnoreCase("headlessfirefox")) {
+			this.driver = getHeadLessFireFox();
+		} else {
+
+			this.driver = executor.openBrowser(host, port, capabilities);
+		}
+
 		wait = new WebDriverWait(driver, 30);
 		executor = new Executioner(driver, wait);
 		landingPage = new LandingPage(driver, wait);
 		util = Util.getInstance();
+	}
+
+	private WebDriver getHeadLessFireFox() {
+		String Xport = System.getProperty("lmportal.xvfb.id", ":1");
+		final File firefoxPath = new File(System.getProperty("lmportal.deploy.firefox.path", "/usr/bin/firefox"));
+		FirefoxBinary firefoxBinary = new FirefoxBinary(firefoxPath);
+		firefoxBinary.setEnvironmentProperty("DISPLAY", Xport);
+		return new FirefoxDriver(firefoxBinary, null);
 	}
 
 	@AfterMethod
